8add4ebc4d39abc38d5c794e8b8e867cc73ac314
saritmg <sakurinkim@gmail.com>
2017-06-16 13:27:42 +0545

    commit

--- courier.sql
+++ courier.sql
@@ -1,13 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 3.5.2.2
+-- version 4.2.11
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 06, 2014 at 10:32 PM
--- Server version: 5.5.27
--- PHP Version: 5.4.7
+-- Generation Time: Jun 16, 2017 at 09:33 AM
+-- Server version: 5.6.21
+-- PHP Version: 5.6.3
 
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
 
@@ -17,10 +17,8 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8 */;
 
 --
--- Database: `courier1`
+-- Database: `courier`
 --
-CREATE DATABASE `courier1` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
-USE `courier`;
 
 -- --------------------------------------------------------
 
@@ -30,9 +28,7 @@ USE `courier`;
 
 CREATE TABLE IF NOT EXISTS `admin` (
   `username` varchar(10) NOT NULL,
-  `password` varchar(10) NOT NULL,
-  PRIMARY KEY (`username`),
-  UNIQUE KEY `password` (`password`)
+  `password` varchar(10) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
@@ -56,8 +52,7 @@ CREATE TABLE IF NOT EXISTS `client` (
   `mobile` varchar(20) NOT NULL,
   `address` varchar(50) NOT NULL,
   `city` varchar(10) NOT NULL,
-  `country` varchar(10) NOT NULL,
-  PRIMARY KEY (`username`)
+  `country` varchar(10) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
@@ -65,10 +60,11 @@ CREATE TABLE IF NOT EXISTS `client` (
 --
 
 INSERT INTO `client` (`username`, `password`, `name`, `emailid`, `mobile`, `address`, `city`, `country`) VALUES
-('ameet', 'ameet', 'Ameet Pradhan', 'jimmy.4244@gmail.com', '9542033378', 'Chandrasekharpur', 'Bhubaneswa', 'India'),
-('ritu', 'ritu', 'Ritu Panda', 'ritu.panda94@gmail.com', '8658443936', 'B - 230, CVR Hall of Residence, NIT Rourkela', 'Rourkela', 'India'),
-('sushovan', 'sushovan', 'Sushovan Das', 'das.susho1@gmail.com', '9658734984', 'B-257, Homi Bhabha Hall of Residence, NIT Rourkela', 'Rourkela', 'India'),
-('om', 'om', 'Om Prakash Acharya', 'omtennislover@gmail.com', '9777889833', 'B-256, Homi Bhabha Hall of Residence, NIT Rourkela', 'Rourkela', 'India');
+('amit', 'amit', 'Amit Pradhan', 'amit@gmail.com', '9542033378', 'Dhapasi', 'Kathmandu', 'Nepal'),
+('ritu', 'ritu', 'Ritu Pandey', 'ritu.p94@gmail.com', '9841652384', 'Balaju', 'Kathmandu', 'Nepal'),
+('suman', 'suman', 'Suman Rai', 'rai.suman@gmail.com', '9658734984', 'Newroad', 'Pokhara', 'Nepal'),
+('kris', 'kris', 'kris wu', 'kris@gmail.com', '9857889833', 'Sejong-daero, Jung-gu', 'Seoul', 'Korea'),
+('alek', 'alek', 'alek tamang', 'alek.t@gmail.com', '9841628952', 'Gongabu', 'Kathmandu', 'Nepal');
 
 -- --------------------------------------------------------
 
@@ -77,7 +73,7 @@ INSERT INTO `client` (`username`, `password`, `name`, `emailid`, `mobile`, `addr
 --
 
 CREATE TABLE IF NOT EXISTS `courier_table` (
-  `cid` int(5) NOT NULL AUTO_INCREMENT,
+`cid` int(5) NOT NULL,
   `orig` varchar(20) NOT NULL,
   `dest` varchar(20) NOT NULL,
   `sname` varchar(20) NOT NULL,
@@ -86,28 +82,32 @@ CREATE TABLE IF NOT EXISTS `courier_table` (
   `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `rate` float NOT NULL,
   `mode` varchar(10) NOT NULL,
-  `status` varchar(20) NOT NULL DEFAULT 'Loaded',
-  PRIMARY KEY (`cid`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=31 ;
+  `status` varchar(20) NOT NULL DEFAULT 'Loaded'
+) ENGINE=MyISAM AUTO_INCREMENT=36 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `courier_table`
 --
 
 INSERT INTO `courier_table` (`cid`, `orig`, `dest`, `sname`, `rname`, `radd`, `date`, `rate`, `mode`, `status`) VALUES
-(19, 'Rourkela', 'Bhubaneswar', 'Anmol', 'Sudhanshu', 'Infocity', '2013-04-14 22:37:01', 100, 'surface', 'In Transit'),
-(18, 'Lucknow', 'Berhampur', 'Manu', 'Swetaswini', 'Hill Patna', '2013-04-14 22:36:00', 2500, 'surface', 'In Transit'),
-(17, 'Rourkela', 'Lucknow', 'Rohit', 'Ritu', 'Gomti Nagar', '2013-04-14 22:34:55', 1600, 'surface', 'Loaded'),
-(16, 'Rourkela', 'Rourkela', 'Rasesh', 'Khirod', 'NIT Rourkela', '2013-04-14 22:33:28', 100, 'surface', 'Delivered'),
-(15, 'Rourkela', 'Bangalore', 'Shivanee', 'Rohan', 'Indiranagar', '2013-04-14 22:32:36', 420, 'air', 'Delivered'),
-(20, 'Rourkela', 'Rourkela', 'Swati', 'Ravi', 'NIT Rourkela', '2013-04-14 22:37:26', 400, 'surface', 'In Transit'),
-(21, 'Rourkela', 'Bangalore', 'Ganesh', 'Sambit', 'Indira Nagar', '2013-04-14 22:37:55', 280, 'air', 'Loaded'),
-(22, 'Rourkela', 'Kandy', 'Sushovan', 'Aditi', 'Kandy, Sri Lanka', '2013-04-14 23:13:30', 400, 'water', 'In Transit'),
-(23, 'Bangalore', 'Bhubaneswar', 'Aditya', 'Chetan', 'Infocity', '2013-04-14 23:36:28', 70, 'air', 'Loaded'),
-(26, 'New Delhi', 'Kolkata', 'Navin', 'Sneha', 'Sealdah', '2013-04-15 14:53:41', 400, 'water', 'Loaded'),
-(28, 'Bangalore', 'Rourkela', 'Manu', 'Sourav', 'Udit Nagar', '2013-04-19 20:51:09', 40, 'air', 'In Transit'),
-(29, 'Lucknow', 'Mumbai', 'Om', 'Uday', 'Dadar', '2014-03-14 15:30:03', 500, 'air', 'Loaded'),
-(30, 'Sky', 'New York', 'Nick Fury', 'Tony Stark', 'Top Floor, Stark Tower', '2014-04-05 15:15:16', 21000, 'air', 'Loaded');
+(1, 'Kathmandu', 'Pokhara', 'Anmol', 'Sudarshan', 'New Road', '2014-10-22 22:37:01', 100, 'surface', 'In Transit'),
+(2, 'Nepal', 'Korea', 'Kris', 'Luhan', 'Seoul city', '2013-04-14 22:36:00', 2500, 'air', 'In Transit'),
+(3, 'Nepal', 'India', 'Rohan', 'Ritu', 'Kolkata', '2013-04-23 22:34:55', 1600, 'surface', 'Loaded'),
+(4, 'Nepal', 'Nepal', 'Riya', 'Puja', 'Rupandehi', '2013-04-14 22:33:28', 100, 'air', 'Delivered'),
+(5, 'China', 'India', 'Minhyuk choi', 'Raja', 'Indiranagar', '2013-04-18 22:32:36', 420, 'water', 'In Transit'),
+(6, 'Nepal', 'Nepal', 'Sweta', 'Ravi', 'Pokhara', '2013-04-24 22:37:26', 400, 'surface', 'Delivered'),
+(7, 'Nepal', 'Thailand', 'Samir', 'Priya', 'Bangkok', '2013-04-14 22:37:55', 280, 'air', 'Loaded'),
+(8, 'China', 'Japan', 'Subin', 'Aditi', 'Tokyo', '2013-04-16 23:13:30', 400, 'water', 'In Transit'),
+(9, 'Nepal', 'Korea', 'Aditya', 'Chiran', 'Seoul city', '2013-04-26 23:36:28', 70, 'air', 'Loaded'),
+(10, 'India', 'Nepal', 'Navina', 'Sneha', 'Butwal', '2013-04-26 14:53:41', 400, 'air', 'Loaded'),
+(11, 'Nepal', 'India', 'Sacar', 'Sourav', 'Udit Nagar', '2013-07-24 20:51:09', 402, 'air', 'In Transit'),
+(12, 'China', 'nepal', 'Om', 'Sambhav', 'Pokhara', '2014-03-14 15:30:03', 500, 'air', 'Loaded'),
+(13, 'Nepal', 'New York', 'Kaanchi', 'David', 'Top Floor, Stark Tower', '2014-05-22 15:15:16', 21000, 'air', 'Loaded'),
+(31, 'Kathmandu', 'Toronta', 'Ganesh', 'James', 'Canada', '2017-06-14 16:17:09', 22400, 'air', 'Loaded'),
+(32, 'Bhaktapur', 'Tokyo', 'Sebika', 'Saron', 'Japan', '2017-06-15 14:46:36', 700, 'air', 'Loaded'),
+(33, 'Bhaktapur', 'Tokyo', 'Sebika', 'Saron', 'Japan', '2017-06-15 14:47:19', 700, 'air', 'Loaded'),
+(34, 'Lalitpur', 'Vancouver', 'Dipa', 'Shiva', 'Canada', '2017-06-15 14:48:57', 1050, 'air', 'Loaded'),
+(35, 'Samakhusi', 'Los Angeles', 'Sarmila', 'Urvashi', 'USA', '2017-06-15 14:53:52', 8000, 'surface', 'Loaded');
 
 -- --------------------------------------------------------
 
@@ -123,8 +123,7 @@ CREATE TABLE IF NOT EXISTS `employee` (
   `mobile` varchar(30) NOT NULL,
   `address` varchar(50) NOT NULL,
   `city` varchar(10) NOT NULL,
-  `country` varchar(10) NOT NULL,
-  PRIMARY KEY (`username`)
+  `country` varchar(10) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
@@ -132,10 +131,48 @@ CREATE TABLE IF NOT EXISTS `employee` (
 --
 
 INSERT INTO `employee` (`username`, `password`, `name`, `emailid`, `mobile`, `address`, `city`, `country`) VALUES
-('rasesh', 'rasesh', 'Rasesh Rout', 'raseshrout@gmail.com', '9439344276', 'B-3, M.V. Hall of Residence, NIT Rourkela', 'Rourkela', 'India'),
-('rohit', 'rohit', 'Rohit Rajat Mohanty', 'rohit.r.mohanty@gmail.com', '8598000238', 'C-1, M.V. Hall of Residence, NIT Rourkela', 'Rourkela', 'India'),
-('manu', 'manu', 'Siddharth Manu', 'siddharthmanu94@gmail.com', '7894428180', 'B-256, Homi Bhabha Hall of Residence, NIT Rourkela', 'Rourkela', 'India');
+('ramesh', 'ramesh', 'Ramesh Joshi', 'ramesh.j@gmail.com', '9439344276', 'SanFransisco', 'California', 'USA'),
+('rohan', 'rohan', 'Rohan Gurung', 'rohan@gmail.com', '9860215482', 'Alaknanda', 'New-delhi', 'India'),
+('Jason', 'jae', 'jason magar', 'jason@gmail.com', '9842568214', 'Kalanki', 'Kathmandu', 'Nepal'),
+('suzy', 'suzy', 'Suzy Choi', 'suzy@gmail.com', '0152698245', '1 Dong Fang Rd,North Dongsanhuan Rd', 'Beijing', 'China');
 
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+ ADD PRIMARY KEY (`username`), ADD UNIQUE KEY `password` (`password`);
+
+--
+-- Indexes for table `client`
+--
+ALTER TABLE `client`
+ ADD PRIMARY KEY (`username`);
+
+--
+-- Indexes for table `courier_table`
+--
+ALTER TABLE `courier_table`
+ ADD PRIMARY KEY (`cid`);
+
+--
+-- Indexes for table `employee`
+--
+ALTER TABLE `employee`
+ ADD PRIMARY KEY (`username`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `courier_table`
+--
+ALTER TABLE `courier_table`
+MODIFY `cid` int(5) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=36;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
